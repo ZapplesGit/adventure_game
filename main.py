2b9e0a0a96cdbe58b145547eee5bdc30c6155eba
ZapplesGit <aidanlmgouldp@gmail.com>
2023-03-05 16:14:32 +1300

    Started on both the inventory and crafting systems.

--- main.py
+++ main.py
@@ -22,16 +22,24 @@ tools and weapons in order to survive and progress. The player must also deal wi
 on a deserted island, facing challenges such as loneliness, fear, and despair.
 '''
 
+
+
 difficulty = 0
 day = 1
-health = 10
-hunger = 10
-action = "n/a"
+health = 5
+hunger = 5
+# action = "n/a"
 health_bar = "n/a"
 hunger_bar = "n/a"
 
+inventory_items = {
+    'rock': 'Found it in the sand... Combine with Stick to make hatchet!',
+    'mre': 'Meal Ready to Eat. Should fill you up.',
+}
 
-def StartGame():
+# inventory_items.update({"example": "example"})
+
+def start_game():
     global difficulty
     print("\nWelcome to Outsider's Odyssey - The Island")
     print("Please select a difficulty level of 1-3")
@@ -48,48 +56,94 @@ def StartGame():
     #time.sleep(1)
     print("You wake up in a foreign land... (SAMPLE INTRODUCTION SEQUENCE)")
     #time.sleep(1)
-    NextAction()
+    next_action()
 
 
-def GetHealth():
+def get_health():
     #♥♡
-    global health
     global health_bar
     health_bar = ("♥"*health + "♡"*(10-health))
 
 
-def GetHunger():
+def get_hunger():
     #🍲✕
-    global hunger
     global hunger_bar
     hunger_bar = ("🍲"*hunger + "✕"*(10-hunger))
 
 
-def Inventory():
-    GetHealth()
-    GetHunger()
+def inventory():
+    get_health()
+    get_hunger()
     print(health_bar)
     print(hunger_bar)
 
+    print(*inventory_items, sep=", ")
 
-def NextAction():
-    global action
-    GetHealth()
-    GetHunger()
+
+#    for keys, value in inventory_items.items():
+#        print(keys)
+
+    print("Type an item to learn more. Type X to exit.")
+
+    while True:
+        action = input("\n> ").lower()
+        if action == "x":
+            break
+        elif action in inventory_items:
+            print(inventory_items[action])
+        else:
+            print("Invalid input.")
+
+    next_action()
+
+def crafting():
+    print("Type an item to use in crafting!")
+    print(*inventory_items, sep=", ")
+    print("Type X to exit.")
+
+    while True:
+        action = input("\n> ").lower()
+        if action == "x":
+            break
+        elif action in inventory_items:
+            print(f"Select a second item to combine with {action}!")
+            action2 = input("\n> ").lower()
+            if action2 == "x":
+                break
+            elif action2 in inventory_items:
+                if action2 != action:
+                    print(action)
+                    print(action2)
+                    print("Operation success")
+                else:
+                    print("Please enter a different item!")
+            else:
+                print("Item not in inventory!")
+        else:
+            print("Item not in inventory!")
+
+
+    next_action()
+
+
+
+def next_action():
+    get_health()
+    get_hunger()
     print(f"Day {day}")
     print(health_bar)
     print(hunger_bar)
-    print("Time is of the essence...")
     print("I - Inventory \nC - Crafting \nE - Exploration \nS - Survival")
+    print("Type an item to use it!")
 
     while True:
         try:
             action = str(input("\n> ").lower())
             if action == "i":
-                Inventory()
+                inventory()
                 break
             elif action == "c":
-                print("Placeholder crafting screen")
+                crafting()
                 break
             elif action == "e":
                 print("Placeholder exploration screen")
@@ -101,5 +155,5 @@ def NextAction():
         except ValueError:
             print("Please enter a string!")
 
-StartGame()
+start_game()
 
