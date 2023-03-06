272c24731f76ea5dee3e7614a5944835cfe1f01a
ZapplesGit <aidanlmgouldp@gmail.com>
2023-03-07 11:50:21 +1300

    Crafting system essentially complete.

--- main.py
+++ main.py
@@ -26,16 +26,21 @@ on a deserted island, facing challenges such as loneliness, fear, and despair.
 
 difficulty = 0
 day = 1
-health = 5
-hunger = 5
+health = 10
+hunger = 10
 # action = "n/a"
 health_bar = "n/a"
 hunger_bar = "n/a"
+combinable = "n/a"
 
 inventory_items = {
-    'rock': 'Found it in the sand... Combine with Stick to make hatchet!',
-    'mre': 'Meal Ready to Eat. Should fill you up.',
+    "rock": "Found it in the sand... Combine with Stick to make hatchet!",
+    "mre": "Meal Ready to Eat. Should fill you up.",
+    "god item": "for testing"
 }
+
+
+
 
 # inventory_items.update({"example": "example"})
 
@@ -96,25 +101,47 @@ def inventory():
 
     next_action()
 
+def combinable_items():
+    global combinable
+    if item1 or item2 == "rock" and item1 or item2 == "mre":
+        combinable = "true"
+        inventory_items.update({"rocky mre": "Not sure if you would want to eat this..."})
+        del inventory_items["rock"]
+        del inventory_items["mre"]
+
+
+
+
+    else:
+        combinable = "false"
+
 def crafting():
+    global item1
+    global item2
     print("Type an item to use in crafting!")
     print(*inventory_items, sep=", ")
     print("Type X to exit.")
 
     while True:
-        action = input("\n> ").lower()
-        if action == "x":
+        item1 = input("\n> ").lower()
+        if item1 == "x":
             break
-        elif action in inventory_items:
-            print(f"Select a second item to combine with {action}!")
-            action2 = input("\n> ").lower()
-            if action2 == "x":
+        elif item1 in inventory_items:
+            print(f"Select a second item to combine with {item1}!")
+            item2 = input("\n> ").lower()
+            if item2 == "x":
                 break
-            elif action2 in inventory_items:
-                if action2 != action:
-                    print(action)
-                    print(action2)
-                    print("Operation success")
+            elif item2 in inventory_items:
+                if item2 != item1:
+                    combinable_items()
+                    if combinable == "true":
+                        print(f"Combining {item1} and {item2}...")
+                        #time.sleep(1)
+                        print(f"Great success! You made a {list(inventory_items)[-1]}.")
+
+
+                    else:
+                        print("These items do not combine!")
                 else:
                     print("Please enter a different item!")
             else:
@@ -125,7 +152,10 @@ def crafting():
 
     next_action()
 
-
+def survival():
+    print("You contemplate your options...")
+    print("H - Hunting \nG - Gathering \nS - SOMETHING")
+    print("Type X to exit.")
 
 def next_action():
     get_health()
@@ -149,7 +179,15 @@ def next_action():
                 print("Placeholder exploration screen")
                 break
             elif action == "s":
-                print("Placeholder survival screen")
+                survival()
+            elif action in inventory_items:
+                print(f"Are you sure you want to use {action}? Y/N")
+                action2 = input("\n> ").lower()
+                if action2 == "y":
+                    #use item
+                    break
+
+
             else:
                 print("Please select a valid choice!")
         except ValueError:
